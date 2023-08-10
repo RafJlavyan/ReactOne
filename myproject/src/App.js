e4c7f8e12b236a64933778a336cc427d4e72b36d
RafJlavyan <jlavyan2021@mail.ru>
2023-08-10 17:32:50 +0400

    loader

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style/App.css";
 import Parent from "./Components/Parent";
 import PostForm from "./Components/PostForm";
@@ -6,23 +6,40 @@ import PostFilter from "./Components/PostFilter";
 import MyModal from "./UI/modal/MyModal";
 import Mybutton from "./UI/button/Mybutton";
 import { usePosts } from "./hooks/usePost";
+import PostService from "./API/PostService";
+import Loader from "./UI/loader/Loader";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [visible, setVisible] = useState("");
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPosteLoading,setIsPostLoading] = useState(false)
+
+  useEffect(() => {
+    fetchPosts()
+  },[])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setVisible(false);
   };
 
+  function fetchPosts() {
+    setIsPostLoading(true)
+    setTimeout(async() => {
+      const posts = await PostService.getAll()
+      setPosts(posts)
+      setIsPostLoading(false)
+    },2000)
+  }
+
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
   return (
     <div className="App">
+      <Mybutton onClick={fetchPosts}>Get Posts</Mybutton>
       <Mybutton style={{ marginTop: "20px" }} onClick={() => setVisible(true)}>
         Create Post
       </Mybutton>
@@ -31,11 +48,13 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <Parent
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title={"Post1"}
-      />
+       {isPosteLoading 
+       ? <div style={{display:'flex',justifyContent:'center',marginTop:'50px'}}><Loader/></div>
+       :<Parent
+       remove={removePost}
+       posts={sortedAndSearchedPosts}
+       title={"Post1"}
+     />}
     </div>
   );
 }
---
+++ myproject/src/UI/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Loader.module.css'
+
+const Loader = ({children}) => {
+  return (
+    <div className={style.loader}>
+        
+    </div>
+  )
+}
+
+export default Loader
